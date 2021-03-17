9cdf1d23e26088ebef413a303b594bb484596ca0
jesssullivan <Jess@sulliwood.org>
2021-03-17 15:00:23 -0400

    adds split training / testing functions and set filtering.

--- preprocess/__main__.py
+++ preprocess/__main__.py
@@ -8,17 +8,21 @@ if __name__ == "__main__":
     fetch & save off the gbif export
     make sure we can load the dwca archive into memory:
     """
-    dwca = MODwca()
+    # dwca = MODwca()
 
     """BuildImages():
     functions to construct image dataset and annotator artefacts 
     """
     buildData = BuildImages()
 
+    """
     buildData.write_images_json()
 
     buildData.fetch_leaflet_tool()
 
     buildData.fetch_online_images(_json=STATIC_PATH + "images.json")
+    """
 
     buildData.export_tgz()
+
+    buildData.split_training_testing()
